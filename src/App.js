d8df6ef3b1d4c1a307fe75068a8eafc915c4d0f5
Mohannad <mohannadalrifai7@gmail.com>
2023-04-23 16:12:25 +0300

    login&register page

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import { Route, Routes } from "react-router-dom";
 import AboutUs from './Components/AboutUs/AboutUs';
 import Cars from './Components/Cars/Cars';
 import ContactUs from './Components/ContactUs/ContactUs';
-import Login from './Components/Login/Login';
+import LoginPage from './Components/Login/Login';
 import Home from './Components/Home/Home';
-import Register from './Components/Auth/Register';
+import Register from './Components/Register/Register';
 function App() {
   return <div className="App">
           <Header />
@@ -16,7 +16,7 @@ function App() {
         <Route path="/AboutUs" element={<AboutUs /> } />
         <Route path="/Cars" element={<Cars />} />
         <Route path="/ContactUs" element={<ContactUs />} />
-        <Route path="/Login" element={<Login />} />
+        <Route path="/Login" element={<LoginPage />} />
         <Route path="/Register" element={<Register />} />
       </Routes>
       <Footer />
