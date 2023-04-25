93cd39b3f67c3657f00d42b73c4590885acbb401
hasan <hasankanj2000@gmail.com>
2023-04-25 14:53:08 +0300

    test

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -12,7 +12,7 @@ function AboutUs() {
   return (
     <Helmet title="About Us">
     <CommonSection title="About Us" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
+    {/* <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/> */}
 
     <div className="about-everything">
       <h5 className="about-welcome"> Welcome to RoadCar</h5>
