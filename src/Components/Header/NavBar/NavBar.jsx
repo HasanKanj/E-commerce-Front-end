e6dfcc05780b4b6923ae6131e1137b0503e6523a
linarawwas <linamkraw2@gmail.com>
2023-04-25 14:39:47 +0200

    recieved

--- src/Components/Header/NavBar/NavBar.jsx
+++ src/Components/Header/NavBar/NavBar.jsx
@@ -1,43 +1,42 @@
-import React from "react";
-import './NavBar.css';
-import {  Link } from "react-router-dom";
+import React from 'react'
+import './navbar.css'
+import { FaBars, FaTimes } from 'react-icons/fa'
+import { useRef } from 'react';
 
-function NavBar() {
+function Navbar() {
+  // this var will refer to nav tag
+  const navRef = useRef();
+
+  //it called when click on buttons
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  }
+
+
   return (
-<>
-<div id="menu_wrapper" class="menu_wrapper">
-  <ul class="menu_list">
-    <li class="list" name="item">
-      <Link to="/" class="home">Home</Link>
+    <header>
+      <h3>Oussama Ghomrawi</h3>
+      <nav ref={navRef}>
 
-    </li>
-   
-    <li class="list" name="item">        
-    <Link to="/Cars" onclick="return 0" name="expand" >Our Cars &#9660;</Link>
-      <ul name="sub_menu" class="submenu_list">
-        <li class="sub_list"> <Link to="/fuelCars" >Fuel Cars</Link></li>
-        <li class="sub_list"> <Link to="/electricCars" >Electric Cars</Link></li>
-      </ul>
-    </li>
-    <li class="list" name="item">
-      <Link to="/AboutUs" >About Us</Link>
+        <a href='#About'>About</a>
+        <a href='#Languages'>Skills</a>
+        <a href='#Projects'>Projects</a>
+        <a href='#ContactSection'>Contact</a>
+        <button
+          className="nav-btn-nav-close-btn"
+          onClick={showNavbar}>
+          <FaTimes />
+        </button>
 
-    </li>
-    <li class="list" name="item">
-    <Link to="/ContactUs" name="single" >Contact Us</Link>
-    </li>
-    
-    <li class="list" name="item">
-    <Link to="/Login" onclick="return 0" name="expand" >Login  &#9660;</Link>
-      <ul name="sub_menu" class="submenu_list">
-        <li class="sub_list sign-up"><Link to='/Register'>Don't Have an Account? Register Here! &#9660; </Link></li>
-      </ul>
-    </li>
-  </ul>
-</div>
-</>  
-    )
+      </nav>
+      {/* to open nav */}
+      <button className="nav-btn"
+        onClick={showNavbar}>
+        <FaBars />
+      </button>
+    </header>
+  );
 }
 
-export default NavBar;
+export default Navbar;
 
