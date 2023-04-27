afdbbc68dac444a00edcb373c2ef1a38b045bc30
linarawwas <linamkraw2@gmail.com>
2023-04-27 15:51:34 +0200

    fixed all the navbar with hover

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,15 +1,25 @@
 import React from "react";
-import fb from './images/Circled_Facebook_svg-512.webp'
-import instagram from './images/instagram.png'
-import './Footer.css'
-function Footer() {
-  return(<div className="footer">
-    <div className="footer-2">
-    <a href="a"><img src={fb} alt="" /></a>
-    <a href="a"><img src={instagram} alt="" /></a>
-    </div>
-    <p>© 2023 ROADCAR. ALL RIGHTS RESERVED CREATED BY MLALH</p> 
-  </div>);
-}
+import ScrollUpButton from "react-scroll-up-button";
+import { BehanceIcon } from "./BehanceIcon";
+import { FacebookIcon } from "./FacebookIcon";
+import { InstagramIcon } from "./InstagramIcon";
+import { LinkdinIcon } from "./LinkdinIcon";
+import { TwitterIcon } from "./TwitterIcon";
 
-export default Footer;
+export const Footer = () => {
+  return (
+    <div className="footer">
+      <p>
+        Copyright &copy; 2023 by Ahmad,Sarah,Fatima,Nour. All Rights Reserved.
+      </p>
+      <div className="icons">
+        <InstagramIcon />
+        <LinkdinIcon />
+        <TwitterIcon />
+        <BehanceIcon/>
+        <FacebookIcon />
+      </div>
+      <ScrollUpButton className="scoll" style={{width: 40,  height:30 }} ToggledStyle={{right: 15}}/>
+    </div>
+  );
+};
